f7ef50756c2deed1350ad4b219446a2efaadc818
leetbir6 <leetbir6@gmail.com>
2025-06-19 17:05:39 +0530

    19th june added test and pom structure

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,41 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+// below line is commented as it is been added in playwright.config.js
+// import dotenv from 'dotenv';
+// dotenv.config(); // calling the config function to load environment variables
+const url = process.env.BASE_URL + "elements";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("T001: Verify Page has title", async ({ page }) => {
+  await page.goto(url);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/DEMOQA/); // /DEMOQA/ is a regex to match the title
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.only("T002: Verify text box element checkpoints", async ({ page }) => {
+  await page.goto(url);
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // Click on Elements in the left sidebar
+  await page.locator("(//ul)[1]").click();
+  // Click on Text Box in the Elements section
+  await page.locator("text=Text Box").click();
+  // check for the Text box div is visible
+  await expect(page.locator('div[class*="col-md-6"]')).toBeVisible();
+  await page.pause();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // // Check if the text box element is visible
+  // const textBox = page.locator('#userName');
+  // await expect(textBox).toBeVisible();
+
+  // // Check if the text box element is enabled
+  // await expect(textBox).toBeEnabled();
+
+  // // Check if the text box element has a placeholder
+  // await expect(textBox).toHaveAttribute('placeholder', 'Full Name');
+
+  // // Check if the text box element has a value
+  // await expect(textBox).toHaveValue('');
+
+  // // Check if the text box element is focused
+  // await expect(textBox).not.toBeFocused();
 });
